90465d9d7d88a528bb4f13b115c83acb0062de89
chenh <chenh@kpy.com>
2021-01-21 00:33:35 +0800

    init login

---
+++ security-common/src/main/java/org/iammrchenh/infrastructure/security/login/config/SecurityLoginConfiguration.java
@@ -0,0 +1,55 @@
+package org.iammrchenh.infrastructure.security.login.config;
+
+import org.iammrchenh.infrastructure.security.login.SecurityCoreProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author chenh
+ * @date 2021/1/18
+ */
+@Order(10)
+@Configuration
+@EnableWebSecurity
+public class SecurityLoginConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SecurityCoreProperties properties;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    /**
+     * http网络请求配置
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //登录配置
+        http.formLogin();
+
+    }
+
+    /**
+     * 认证管理配置
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+
+}
